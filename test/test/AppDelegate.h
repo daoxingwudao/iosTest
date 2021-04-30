1d7b888a6d3ec8c1171aaa3d8507eadd3b90bf02
daoxingwudao <daoxingwudao@163.com>
2021-04-30 16:57:29 +0800

    create

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 卢逢卫 on 2021/4/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 卢逢卫 on 2021/4/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
